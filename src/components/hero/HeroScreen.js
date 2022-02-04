e95c121ac6557f2f49cc011373c3c0bd8a1c3b11
JorgeMano <jorge.l.manosalva@gmail.com>
2022-02-03 23:14:33 -0500

    Frist commit

---
+++ src/components/hero/HeroScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useParams, Navigate } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
+
+export const HeroScreen = () => {
+
+  const { id } = useParams();
+  
+  const hero = getHeroById(id);
+
+  if(!hero){
+    return <Navigate to="/" />
+  }
+
+  return(
+      <div>
+          <h1>Hero Screen</h1>
+          <p>
+            { hero.superhero }
+          </p>
+      </div>
+  )
+};
---
+++ src/components/login/LoginScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const LoginScreen = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    //console.log('handleLogin');
+    navigate('/marvel', {
+      replace: true
+    });
+  }
+
+  return(
+      <div className='container mt-5'>
+          <h1>LoginScreen</h1>
+          <hr />
+
+          <button 
+            className='btn btn-primary'
+            onClick={ handleLogin }
+            >
+              Log in
+          </button>
+      </div>
+  )
+};
